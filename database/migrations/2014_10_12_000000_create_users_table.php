0d72884fec9d481806b3e7059b7e2289f726f4fc
AdityaSingh0 <aditya.notification@gamil.com>
2021-02-23 20:38:41 +0530

    Updating user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('e_code')->unique();
-            $table->integer('mobile')->unique();
+            $table->string('mobile')->unique();
             $table->integer('user_type')->default(2)->comment('1 = Admin 2 = Customer');
             $table->tinyInteger('status')->default(1)->comment('1 = Active, 0 = Inactive');
             $table->timestamp('email_verified_at')->nullable();
