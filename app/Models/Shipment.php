8b399e198136cb14acc0313c0ca79ba7fec1029a
AdityaSingh0 <36704696+AdityaSingh0@users.noreply.github.com>
2021-02-23 14:27:30 +0530

    Updating shipment model

---
+++ app/Models/Shipment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shipment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'batch_number',
+        'order_number',
+        'arrived_id',
+        'date',
+        'ship_type',
+        'status',
+        'description'
+    ];
+}
