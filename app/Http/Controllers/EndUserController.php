921f3774a392034fb3e5a528200601d247cd2fd3
AdityaSingh0 <36704696+AdityaSingh0@users.noreply.github.com>
2021-02-26 18:40:38 +0530

    updating controller to create dynamic listing

--- app/Http/Controllers/EndUserController.php
+++ app/Http/Controllers/EndUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Models\Arrived;
 
 class EndUserController extends Controller
 {
@@ -28,6 +31,15 @@ class EndUserController extends Controller
      * 
      */
     function arrived() {
-        return view('end_user.arrived');
+        try{
+            $arrived = array();
+            $arrived = Arrived::whereCustomerCode(Auth::user()->e_code)->get();
+            if(count($arrived)){
+                $arrived = $arrived->toArray();
+            }
+        } catch (\Exception $ex){
+            Log::error('Error in Customer->index() '. $ex->getMessage(). '\n');
+        }
+        return view('end_user.arrived',array('all_arrived' => $arrived));
     }
 }
