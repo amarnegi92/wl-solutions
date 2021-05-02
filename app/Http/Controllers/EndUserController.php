7a0628b8304d71d5a3c057f026e371600d30454d
amarnegi92 <amarnegi.negi29@gmail.com>
2021-05-02 13:29:42 +0530

    updated order by desc on created_at for customer

--- app/Http/Controllers/EndUserController.php
+++ app/Http/Controllers/EndUserController.php
@@ -5,13 +5,10 @@ namespace App\Http\Controllers;
 use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use App\Models\Arrived;
 use App\Models\Order;
-use App\Models\Shipment;
 use App\Models\Transport;
 use App\Models\User;
 use Validator;
-use Redirect;
 
 class EndUserController extends Controller
 {
@@ -59,20 +56,8 @@ class EndUserController extends Controller
         try {
             $arrived = array();
             $userBatch = array();
-            $arrived = Transport::where('user_id', Auth::user()->id)->where('ship_status', '!=', config('shipment.status.arrived'))->where('ship_status', '!=', config('shipment.status.delivered'))->whereShipType(config('shipment.transport.sea'))->get();
-            /*if (count($arrived)) {
-                $arrived = $arrived->toArray();
-                $shipments = Shipment::whereIn('order_number', array_keys($arrived))->whereShipType(2)->get();
-                if (count($shipments)) {
-                    $shipments = $shipments->toArray();
-                    foreach ($shipments as $value) {
-                        $userBatch[$value['batch_number']]['order_number'][] = $value['order_number'];
-                        $userBatch[$value['batch_number']]['date'] = $value['date'];
-                        $userBatch[$value['batch_number']]['description'] = $value['description'];
-                        $userBatch[$value['batch_number']]['status'] = $value['status'];
-                    }
-                }
-            }*/
+            $arrived = Transport::where('user_id', Auth::user()->id)->where('ship_status', '!=', config('shipment.status.arrived'))->where('ship_status', '!=', config('shipment.status.delivered'))->whereShipType(config('shipment.transport.sea'))
+            ->orderBy('created_at', 'DESC')->get();
         } catch (\Exception $ex) {
             Log::error('Error in Customer->index() ' . $ex->getMessage() . '\n');
             dd($ex->getMessage());
@@ -89,21 +74,8 @@ class EndUserController extends Controller
         try {
             $arrived = array();
             $userBatch = array();
-            $arrived = Transport::where('user_id', Auth::user()->id)->where('ship_status', '!=', config('shipment.status.arrived'))->where('ship_status', '!=', config('shipment.status.delivered'))->whereShipType(config('shipment.transport.air'))->get();
-            /*if (count($arrived)) {
-                $arrived = $arrived->toArray();
-                $shipments = Shipment::whereIn('order_number', array_keys($arrived))->whereShipType(1)->get();
-                if (count($shipments)) {
-                    $shipments = $shipments->toArray();
-
-                    foreach ($shipments as $value) {
-                        $userBatch[$value['batch_number']]['order_number'][] = $value['order_number'];
-                        $userBatch[$value['batch_number']]['date'] = $value['date'];
-                        $userBatch[$value['batch_number']]['description'] = $value['description'];
-                        $userBatch[$value['batch_number']]['status'] = $value['status'];
-                    }
-                }
-            }*/
+            $arrived = Transport::where('user_id', Auth::user()->id)->where('ship_status', '!=', config('shipment.status.arrived'))->where('ship_status', '!=', config('shipment.status.delivered'))->whereShipType(config('shipment.transport.air'))
+            ->orderBy('created_at', 'DESC')->get();
         } catch (\Exception $ex) {
             Log::error('Error in Customer->index() ' . $ex->getMessage() . '\n');
             dd($ex->getMessage());
@@ -119,7 +91,8 @@ class EndUserController extends Controller
         try {
             $orders = array();
             $orders = Order::where('user_id', Auth::user()->id)
-                ->whereNotIn('status', [config('package.keyState.shipped')])->get();
+                ->whereNotIn('status', [config('package.keyState.shipped')])
+                ->orderBy('created_at', 'DESC')->get();
             if (count($orders)) {
                 $orders = $orders->toArray();
             }
@@ -135,7 +108,8 @@ class EndUserController extends Controller
     function arrived()
     {   $shipmentStatus = config('shipment.status');
         $all_arrived = Transport::where('user_id', Auth::user()->id)
-                ->whereIn('ship_status', [$shipmentStatus['arrived'], $shipmentStatus['delivered']])->get();
+                ->whereIn('ship_status', [$shipmentStatus['arrived'], $shipmentStatus['delivered']])
+                ->orderBy('created_at', 'DESC')->get();
         return view('end_user.arrived', array('all_arrived' => $all_arrived));
     }
 }
