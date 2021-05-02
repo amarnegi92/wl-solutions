10cfd940543ccc1d3bd990677cc217f361ca0405
amarnegi92 <amarnegi.negi29@gmail.com>
2021-05-02 17:15:37 +0530

    added arrived at column to arrived page

--- resources/views/admin/arrived.blade.php
+++ resources/views/admin/arrived.blade.php
@@ -18,6 +18,7 @@
                     <table width="100%" class="table table-hover" id="dataTables-example">
                         <thead>
                             <tr>
+                                <th class="d-none"></th>
                                 <th>Shipped Via</th>
                                 <th>CTN Qty.</th>
                                 <th>Received on</th>
@@ -26,14 +27,15 @@
                                 <th>Weight</th>
                                 <th>ETA</th>
                                 <th>Container</th>
-                                
                                 <th>Status</th>
+                                <th>Arrived At</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
                             foreach ($transports as $package) { ?>
                                 <tr>
+                                    <td class="d-none"><?= strtotime(now()) - strtotime($package['created_at']); ?></td>
                                     <td>
                                         <h5><span class="badge {{ config('shipment.badges.' . $package->ship_type) }}">
                                         {{ ucwords(config('shipment.keyTransport.' . $package->ship_type)) }}
@@ -52,6 +54,16 @@
                                         <h5>
                                             <span class="badge {{ config('shipment.badges.' .$package->ship_status) }}">{!! config('shipment.keyStatus.' .$package->ship_status) !!}</span>
                                         </h5>
+                                    </td>
+                                    <td class="text-center">
+                                        @if ( (config('shipment.status.arrived') ==  $package->ship_status) &&  $package->arrived_at )
+                                            <span class="badge badge-secondary">
+                                                {{ date('d M Y', strtotime($package->arrived_at)) }}
+                                                <br>{{ date('H:i a', strtotime($package->arrived_at)) }}
+                                            </span>
+                                        @else
+                                            N/A
+                                        @endif
                                     </td>
                                 </tr>
                             <?php }
